688f77cabfc6e0d11fb219886948f21d00767dd3
Emre Ozan Memis <63871298+EmreOzanMemis@users.noreply.github.com>
2024-11-15 11:49:45 +0300

    Update Ubuntu-VM-Olusturma.ps1

--- Ubuntu-VM-Olusturma.ps1
+++ Ubuntu-VM-Olusturma.ps1
@@ -1,13 +1,29 @@
+# Parameters
 $resourceGroup = "AzureRG"
 $location = "westus"
-$gatewaysubnet = New-AzVirtualNetworkSubnetConfig -Name PSGatewaySubnet -AddressPrefix "10.172.100.0/27"
-$virtualNetwork = New-AzVirtualNetwork -Name PSAzureBCVnet -ResourceGroupName AzureRG -Location westus -AddressPrefix "10.172.100.0/24" -Subnet $gatewaysubnet
-Add-AzVirtualNetworkSubnetConfig -Name AzureFirewallSubnet -VirtualNetwork $virtualNetwork -AddressPrefix "10.172.100.32/27"
-Add-AzVirtualNetworkSubnetConfig -Name AzureBastionSubnet -VirtualNetwork $virtualNetwork -AddressPrefix "10.172.100.64/27"
-Add-AzVirtualNetworkSubnetConfig -Name PSDMZSubnet -VirtualNetwork $virtualNetwork -AddressPrefix "10.172.100.96/27"
-$virtualNetwork | Set-AzVirtualNetwork
-$publicipvm = New-AzPublicIpAddress -ResourceGroupName "AzureRG" -name "VMIP" -location "westus" -AllocationMethod Static -Sku Standard
-$vmName = "vm-docker"
-$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create Resource Group
 New-AzResourceGroup -Name $resourceGroup -Location $location
-New-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Location $location -Image "Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest" -Size "Standard_B4ms" -VirtualNetworkName "PSAzureBCVnet" -SubnetName "PSDMZSubnet" -PublicIpAddressName $publicipvm -Credential $cred -OpenPorts 22
+
+# Create Gateway Subnet Configuration
+$gatewaySubnet = New-AzVirtualNetworkSubnetConfig -Name PSGatewaySubnet -AddressPrefix "10.172.100.0/27"
+
+# Create Virtual Network and Subnets
+$virtualNetwork = New-AzVirtualNetwork -Name PSAzureBCVnet -ResourceGroupName $resourceGroup -Location $location -AddressPrefix "10.172.100.0/24" -Subnet $gatewaySubnet
+$virtualNetwork | Add-AzVirtualNetworkSubnetConfig -Name AzureFirewallSubnet -AddressPrefix "10.172.100.32/27" | Out-Null
+$virtualNetwork | Add-AzVirtualNetworkSubnetConfig -Name AzureBastionSubnet -AddressPrefix "10.172.100.64/27" | Out-Null
+$virtualNetwork | Add-AzVirtualNetworkSubnetConfig -Name PSDMZSubnet -AddressPrefix "10.172.100.96/27" | Out-Null
+$virtualNetwork | Set-AzVirtualNetwork
+
+# Create Public IP Address
+$publicIp = New-AzPublicIpAddress -ResourceGroupName $resourceGroup -Name "VMIP" -Location $location -AllocationMethod Static -Sku Standard
+
+# Create Credentials for the VM
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create Virtual Machine
+$vmName = "vm-docker"
+New-AzVM -ResourceGroupName $resourceGroup -Name $vmName -Location $location `
+    -Image "Canonical:0001-com-ubuntu-server-jammy:22_04-lts:latest" -Size "Standard_B4ms" `
+    -VirtualNetworkName "PSAzureBCVnet" -SubnetName "PSDMZSubnet" -PublicIpAddressName $publicIp.Name `
+    -Credential $cred -OpenPorts 22
